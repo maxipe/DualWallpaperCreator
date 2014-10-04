0cb0c546853ec9776b1bcaa705c7ba63bb282d98
maxipe <maxipenia@hotmail.com>
2014-10-03 21:31:55 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+
+*** Dual Screen Wallpaper Creator ***
+Creates a new image out of other two concatenating them so it can be used as a wallpaper for two screens.
+
+TODO:
++ Interactive menu.
++ Automatically set wallpaper.
+	-eg: gsettings set org.gnome.desktop.background picture-uri 'file:///home/user/Documents/Pictures/example.jpg'
++ Look for files.
++ Custom size for screen.
++ Find screen sizes automatically.
++ Receive image names.
++ Adapt for different screen layouts (on top/below).
++ Allow more screens.
++ Allow random image selection and creation.
++ Analize what to do if image doesn't have the same ratio as destination screen.
++ Allow to center image if it is smaller than screen.
---
+++ WallpaperCreator.py
@@ -0,0 +1,58 @@
+from PIL import Image
+
+def getWallpaperSize(firstResolution, SecondResolution):
+	""" First and Second resolutions should be tuples
+	with width and height. """
+	return (firstResolution[0] + secondResolution[0],
+		max(firstResolution[1],secondResolution[1]) + verticalOffset)
+
+def copy(destPixels, source, xOffset = 0, yOffset = 0):
+	"""Source should fit in dest.
+	Dest is a loaded image ( image.load() )
+	Source is just an open image (image.open() )
+	xOffset is N pixels from left side.
+	yOffset is N pixels from top. """
+
+	width, height = source.size
+
+	sourcePixels = source.load()
+	for x in xrange(width):
+		for y in xrange(height):
+			try:
+				destPixels[x + xOffset, y + yOffset] = sourcePixels[x,y]
+			except:
+				pass
+
+
+if __name__ == "__main__":
+	firstResolution = (1920,1080)
+	secondResolution = (1280,1024)
+	verticalOffset = 0 #Second screen is N pixels below first screen
+	
+	firstImageName = "1.jpg"
+	secondImageName = "2.jpg"
+
+	firstImage = Image.open(firstImageName)
+	secondImage = Image.open(secondImageName)
+
+	#Resizing images to fit new one.	
+	firstImage = firstImage.resize(firstResolution, Image.ANTIALIAS)
+	secondImage = secondImage.resize(secondResolution, Image.ANTIALIAS)	
+
+	wallpaperSize = getWallpaperSize(firstResolution, secondResolution)
+
+	newImage = Image.new('RGB', wallpaperSize,(123,123,123))
+	
+	"""
+	firstImagePixels = firstImage.load()
+	secondImagePixels = secondImage.load()
+	"""
+	newImagePixels = newImage.load()
+
+	copy(newImagePixels, firstImage)
+	copy(newImagePixels, secondImage, firstResolution[0], verticalOffset)
+	
+	newImage.save("prueba.jpg")
+else:
+	pass
+	#ToDo
