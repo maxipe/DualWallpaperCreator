9773b8a1fa0c032735a518c8f7214e01dd389eea
maxipe <maxipenia@hotmail.com>
2014-10-10 05:21:49 -0300

    Refactor.

--- WallpaperCreator.py
+++ WallpaperCreator.py
@@ -1,6 +1,10 @@
 from PIL import Image
+import sys
 
-def getWallpaperSize(firstResolution, SecondResolution):
+def logError(errorMessage):
+	print errorMessage
+	
+def getWallpaperSize(firstResolution, secondResolution, verticalOffset = 0):
 	""" First and Second resolutions should be tuples
 	with width and height. """
 	return (firstResolution[0] + secondResolution[0],
@@ -23,23 +27,28 @@ def copy(destPixels, source, xOffset = 0, yOffset = 0):
 			except:
 				pass
 
-
-if __name__ == "__main__":
-	firstResolution = (1920,1080)
-	secondResolution = (1280,1024)
-	verticalOffset = 0 #Second screen is N pixels below first screen
+def openImage(imagePath):
+	try:
+		return Image.open(imagePath)
+	except IOError:
+		logError("\""+imagePath+"\""+" is not a valid image file.")
+		
+def createWallpaper(firstImagePath, secondImagePath, saveName, firstResolution, secondResolution, verticalOffset = 0):
+	""" First and second images paths.
+	Resolution of both screens	
+	"""
+	firstImage = openImage(firstImagePath)
+	secondImage = openImage(secondImagePath)
 	
-	firstImageName = "1.jpg"
-	secondImageName = "2.jpg"
-
-	firstImage = Image.open(firstImageName)
-	secondImage = Image.open(secondImageName)
+	if (not firstImage) or (not secondImage):
+		logError("Invalid image file. Stoped.")
+		return
 
 	#Resizing images to fit new one.	
 	firstImage = firstImage.resize(firstResolution, Image.ANTIALIAS)
 	secondImage = secondImage.resize(secondResolution, Image.ANTIALIAS)	
 
-	wallpaperSize = getWallpaperSize(firstResolution, secondResolution)
+	wallpaperSize = getWallpaperSize(firstResolution, secondResolution, verticalOffset)
 
 	newImage = Image.new('RGB', wallpaperSize)
 	
@@ -52,7 +61,27 @@ if __name__ == "__main__":
 	copy(newImagePixels, firstImage)
 	copy(newImagePixels, secondImage, firstResolution[0], verticalOffset)
 	
-	newImage.save("wallpaper.jpg")
+	newImage.save(saveName)
+
+def main(arguments):
+	firstResolution = (1920,1080)
+	secondResolution = (1280,1024)
+	verticalOffset = 0 #Second screen is N pixels below first screen
+	
+	if len(arguments) >= 3:
+		firstImageName = arguments[1]
+		secondImageName = arguments[2]
+	else:
+		firstImageName = "1.jpg"
+		secondImageName = "2.jpg"
+
+	saveName = "wallpaper.jpg"
+	
+	createWallpaper(firstImageName, secondImageName, saveName, firstResolution, secondResolution, verticalOffset)
+	
+if __name__ == "__main__":
+	main(sys.argv)
+		
 else:
 	pass
 	#ToDo
